4a2c6d43a2bd324f921e61008df56d12ffdaf09e
Shirley <hu38@bu.edu>
2019-08-03 17:23:13 -0400

    progress on account

--- src/main/java/Model/Customer.java
+++ src/main/java/Model/Customer.java
@@ -5,5 +5,7 @@ public class Customer extends Trader{
     private CheckingAccount checkingAccount;
     private SavingAccount savingAccount;
     private SecurityAccount securityAccount;
+    private Transaction transaction;
 
+    
 }
